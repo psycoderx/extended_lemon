6ca98571289da489b2ad542a804b35e6fa661a2f
Artem Pirunov <psycoderx@gmail.com>
2024-06-15 13:27:12 +0300

    Add LICENSE info to xlx and xlas

--- xlx.c
+++ xlx.c
@@ -1,6 +1,10 @@
 /*
-BUILD:
+xlx.c - is a simple XL powered virtual vachine.
+See LICENSE information in the end of the file.
+  BUILD
 gcc -std=c89 -pedantic -Wall -Wextra -o xlx xlx.c
+  RUN
+xlx <input-files...>
 */
 
 #include <assert.h>
@@ -154,4 +158,28 @@ errf(const char *fmt, ...)
 
 #define EXTENDED_LEMON_C
 #include "extended_lemon.h"
+
+/*
+MIT License
+
+Copyright (c) 2024 Artem Pirunov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
